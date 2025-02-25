8809bfd18e64b456d3bfcc76aa91a1ebc50f925f
Drtikolnik <drtluk00@gasos-ro.cz>
2025-02-25 07:48:39 +0100

    snad

--- src/autosaloon/Car.java
+++ src/autosaloon/Car.java
@@ -2,5 +2,19 @@ package autosaloon;
 
 public class Car {
 
+    private String rokVyroby;
+    private int pocetKm;
+    private String barva;
+    private String stav;
+
+    public Car(String rokVyroby, int pocetKm, String barva, String stav) {
+        this.rokVyroby = rokVyroby;
+        this.pocetKm = pocetKm;
+        this.barva = barva;
+        this.stav = stav;
+    }
+
+
+
 	
 }
